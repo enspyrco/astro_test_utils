8000261d02b72f63574ac9903c5800455151b0fc
Nick Meinhold <nick@enspyr.co>
2023-06-22 02:19:36 +1000

    Upgrade package dependencies, remove firestore stub
    I removed stubbed_firestore_service.dart and added a task to consider
    how we could organise the test utils, eg. should we have separate
    packages for test utils for astro_core, astro_firestore, etc.

--- lib/astro_widgets_test_utils.dart
+++ lib/astro_widgets_test_utils.dart
@@ -1,7 +1,6 @@
 library astro_test_utils;
 
 export 'src/recording_mission_control.dart';
-export 'src/services/stubbed_firestore_service.dart';
 export 'src/widget_test_harness.dart';
 export 'src/state/user_auth_state.dart';
 export 'src/state/auth_state.dart';
